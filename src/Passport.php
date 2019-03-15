501c7793800cb646dc84ebd21acc6322fe8a6a57
Tylerian <myself@jairo.me>
2019-03-15 14:54:27 +0100

    Proxy incompatible calls through Passport superclass.

---
+++ src/Passport.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tylerian\Lumen\Passport;
+
+use Laravel\Passport\Passport as AbstractPassport;
+use Tylerian\Lumen\Support\Proxies\RouterProxy;
+
+class Passport extends AbstractPassport
+{
+    /**
+     * Binds the Passport routes into the controller.
+     *
+     * @param  callable|null  $callback
+     * @param  array  $options
+     * @return void
+     */
+    public static function routes($callback = null, array $options = [])
+    {
+        $callback = $callback ?: function ($router) {
+            $router->all();
+        };
+
+        $defaultOptions = [
+            'prefix' => 'oauth',
+            'namespace' => '\Laravel\Passport\Http\Controllers',
+        ];
+
+        $options = array_merge($defaultOptions, $options);
+
+        Route::group($options, function ($router) use ($callback) {
+            $callback(new RouteRegistrar(new RouterProxy($router)));
+        });
+    }
+}
